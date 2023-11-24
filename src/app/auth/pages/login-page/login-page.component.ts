e047a20b86b4e2a424fe679f39a42ea1e21b1d2c
Juan Luna <lunacalvojuanignacio@gmail.com>
2023-11-24 12:00:00 -0300

    Continuando con el Proyecto. Add Login 24/11/2023

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +8,17 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
+
 export class LoginPageComponent {
+
+  constructor (private authService:AuthService, private router:Router){}
+
+  onLogin():void {
+    this.authService.login('juanignacioluna@gmail.com','123456')
+      .subscribe( user => {
+
+        this.router.navigate(['/']);
+      })
+  }
 
 }
